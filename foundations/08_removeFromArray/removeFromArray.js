8fb6407831e6ad2e3533f7c71e21a0d9fe7870d2
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-15 16:47:14 +0100

    Add js foundations exo 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...numbers) {
+  if (numbers.length === 0) {
+    return arr;
+  }
+  let res = arr.slice();
+  for (let num of numbers) {
+    res = res.filter((item) => item !== num);
+  }
+  return res;
 };
 
 // Do not edit below this line
