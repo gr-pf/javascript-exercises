fdddc6beb9133231cfa206934ff94957b64ab059
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-20 14:21:16 +0100

    Add js foundations exo 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+// const fibonacci = function (n) {
+//   if (+n === 0) {
+//     return 0;
+//   } else if (+n < 0) {
+//     return "OOPS";
+//   } else if (+n === 1 || +n === 2) {
+//     return 1;
+//   } else {
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+// };
 
-};
+function fibonacci(n) {
+  let cur = 1;
+  let prec = 0;
+  let tmp;
+  if (+n < 0) {
+    return "OOPS";
+  } else if (+n === 0) {
+    return 0;
+  } else {
+    for (let i = 1; i < +n; i++) {
+      tmp = cur;
+      cur = cur + prec;
+      prec = tmp;
+    }
+  }
+  return cur;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
