5b322fba5577321cdd2d4bd04eeb99ad34119d92
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-20 14:04:56 +0100

    Add js foundations exo 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let cleanText = text
+    .replaceAll(" ", "")
+    .replaceAll(",", "")
+    .replaceAll(".", "")
+    .replaceAll("!", "")
+    .toLowerCase()
+    .split("");
 
+  let reverseText = cleanText.slice().reverse();
+
+  for (let i = 0; i < cleanText.length; i++) {
+    if (cleanText[i] !== reverseText[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
