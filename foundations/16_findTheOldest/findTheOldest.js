09747b8c62107635c7af9404b62363a43c90189c
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-20 15:05:19 +0100

    Add js foundations exo 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const thisYear = new Date().getFullYear();
+  const arrByAge = arr.toSorted(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      return b.yearOfDeath - b.yearOfBirth - (thisYear - a.yearOfBirth);
+    } else if (b.yearOfDeath === undefined) {
+      return thisYear - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+    } else {
+      return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+    }
+  });
+  return arrByAge[0];
 };
 
 // Do not edit below this line
