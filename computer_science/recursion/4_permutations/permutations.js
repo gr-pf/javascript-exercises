29d193b10d9b3c8100ee9d9696f4aec20fde8d53
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-04 19:25:32 +0200

    Add CS Recursion 04

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,18 @@
-const permutations = function() {
-  
+/**
+ *
+ * @param {Array} arr
+ * @returns
+ */
+const permutations = function (arr) {
+  if (arr.length === 0) {
+    return [[]];
+  }
+  return arr.flatMap((e) => {
+    const reste = arr.filter((el) => el !== e);
+    const sousPerm = permutations(reste);
+    return sousPerm.map((el) => [e, ...el]);
+  });
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
