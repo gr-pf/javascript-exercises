719489c5c0128c1d43c1b24f63a626d801eb15c5
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-04 18:02:36 +0200

    Add CS Recursion 03

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,27 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  if (
+    ["function", "number", "string", "undefined", "boolean"].includes(
+      typeof obj,
+    ) ||
+    obj === null
+  ) {
+    return;
+  }
+  let total = 0;
+  for (const key of Object.keys(obj)) {
+    if (typeof obj[key] === "number") {
+      if (!isNaN(obj[key]) && Number.isInteger(obj[key])) {
+        total++;
+      }
+    } else {
+      const subTotal = totalIntegers(obj[key]);
+      if (subTotal) {
+        total += subTotal;
+      }
+    }
+  }
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
