60352585c284e36f9736c6ec3fdc43e3c2d33f00
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-04 19:35:15 +0200

    Add CS Recursion 05

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,14 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+  const baseArr = [0, ...pascal(n - 1), 0];
+  const newArr = [];
+  for (let i = 0; i < baseArr.length - 1; i++) {
+    newArr.push(baseArr[i] + baseArr[i + 1]);
+  }
+  return newArr;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
