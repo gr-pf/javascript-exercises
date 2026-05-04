32e72cfec8fbed6cc48af5f3ebf04eeff50e026f
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-04 17:26:23 +0200

    Add CS Recursion 02

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,22 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  let res = false;
+  if (
+    ["number", "string", "undefined", "boolean"].includes(typeof obj) ||
+    obj === null
+  ) {
+    if (typeof obj === "number" && typeof value === "number" && isNaN(value)) {
+      return res || isNaN(obj);
+    }
+    return res || obj === value;
+  }
+  for (const [key, val] of Object.entries(obj)) {
+    res = res || contains(val, value);
+    if (val === value) {
+      return true;
+    }
+  }
+  return res;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
